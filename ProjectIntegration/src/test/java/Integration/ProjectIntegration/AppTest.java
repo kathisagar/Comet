447e9982b773de95930081beb0f385296ff988ec
kdayasag <kdayasag@LH7U05CG51707G9.uhc.com>
2018-02-28 15:12:02 +0530

    sauce test

--- ProjectIntegration/src/test/java/Integration/ProjectIntegration/AppTest.java
+++ ProjectIntegration/src/test/java/Integration/ProjectIntegration/AppTest.java
@@ -1,11 +1,15 @@
 package Integration.ProjectIntegration;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -13,13 +17,17 @@ import org.testng.annotations.Test;
  
 public class AppTest {
 
-	public static void  print() throws InterruptedException{
+	public static void  print() throws InterruptedException, MalformedURLException{
 		
-/*		System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
-		DesiredCapabilities capabilities=DesiredCapabilities.internetExplorer();
-		capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();              
+		capabilities.setCapability("platform", "Windows 7");               
+		capabilities.setCapability("version", "45"); 
+		capabilities.setCapability("idleTimeout", 180);  
 		capabilities.setCapability("requireWindowFocus", true);
-		WebDriver driver = new InternetExplorerDriver(capabilities);
+
+		WebDriver driver = new RemoteWebDriver(
+		new URL("http://"+System.getenv("SAUCE_USERNAME")+":"+System.getenv("SAUCE_ACCESS_KEY")+"@ondemand.saucelabs.com:80/wd/hub"),
+		capabilities);
 		
 		driver.get("http://apsrt3981.uhc.com:8080/prweb/PRServlet");
 		driver.findElement(By.id("txtUserID")).sendKeys("cometwfa");
@@ -27,7 +35,7 @@ public class AppTest {
 		driver.findElement(By.className("loginButton")).click();
 		driver.manage().window().maximize();
 	
-		driver.close();*/
+		driver.close();
 		
 		
 		System.out.println("Testing with Jenkins");
@@ -38,7 +46,7 @@ public class AppTest {
 
   @Test
   
-  public void testng() throws InterruptedException{
+  public void testng() throws InterruptedException, MalformedURLException{
 	  print();
 	  
   }
